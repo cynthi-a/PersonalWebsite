6dee8dbbc1e1792beda56798e6de3527dba1077a
Cynthia Lee <cynthia@cynthialee.xyz>
2018-11-09 08:59:38 +0000

    make 404 page image not overflow for mobile

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 const NotFoundPage = () => (
   <div>
-    <h1>404 - Page not found</h1>
-    
+    <h1>Page not found</h1>
     <img src="https://httpstatusdogs.com/img/404.jpg" alt="404 page not found" />
   </div>
 )
